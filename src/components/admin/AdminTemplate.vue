19abe12cf72cdc7289dec42388f94aa40ff8495b
Shawn Huang <shawn.huang.y.h@gmail.com>
2023-04-27 19:28:25 +0800

    feat: add template(using slot)

---
+++ src/components/admin/AdminTemplate.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside class="nav-bar">
+        <admin-nav-bar />
+      </el-aside>
+      <el-main>
+        <slot />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script setup>
+import AdminNavBar from "@/components/admin/AdminNavBar.vue";
+</script>
+
+<style scoped>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.nav-bar {
+  color: #333;
+  width: 200px;
+  min-height: 100%;
+}
+</style>
