39551cf779391d8f6ee134007798aae4374afd20
Shawn Huang <shawn.huang.y.h@gmail.com>
2023-05-04 23:48:04 +0800

    feat: add router guard

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { getToken } from "@/utils/token";
+import { getRole, getToken, getUser, rmToken } from "@/utils/token";
 import { ElMessage } from "element-plus";
 import Login from "@/components/Login.vue";
 import Class from "@/components/admin/Class.vue";
@@ -47,50 +47,98 @@ const router = createRouter({
       name: "Department",
       component: Department,
     },
+    {
+      path: "/student",
+      name: "Department",
+      component: Department,
+    },
   ],
 });
 
-// router.beforeEach(async (to, from, next) => {
-//   // 用户已经登陆
-//   if (getToken()) {
-//     // 前往登陆界面
-//     if (to.path === "/") {
-//       ElMessage({
-//         type: "warning",
-//         message: "已经登录，不能重复登录",
-//       });
-//       next(from.path);
-//     } else {
-//       //判断
-//       if (useAppStore().user.userInfo.username) {
-//         next();
-//       } else {
-//         //用户登录后获取用户信息
-//         try {
-//           const result = await useAppStore().dispatch("user/getUserInfo");
-//           if (result) {
-//             next();
-//           }
-//         } catch (e) {
-//           //token异常了，就清除token
-//           await useAppStore().dispatch("user/logout");
-//           console.log(e.message);
-//           next("/login");
-//         }
-//       }
-//     }
-//   } else {
-//     //未登录状态下
-//     if (to.path.indexOf("/post-article") !== -1) {
-//       ElMessage({
-//         duration: 1000,
-//         message: "当前尚未登录，请先登录",
-//       });
-//       next("/login");
-//     } else {
-//       next();
-//     }
-//   }
-// });
+router.beforeEach(async (to, from, next) => {
+  // 用户已经登陆
+  if (getToken()) {
+    const role = getRole();
+    let index;
+    switch (role) {
+      case "ADMIN":
+        index = "/admin/course";
+        break;
+      case "STUDENT":
+        index = "/student";
+        break;
+      case "TEACHER":
+        index = "/teacher";
+        break;
+    }
+    // 前往登陆界面
+    if (to.path === "/") {
+      ElMessage({
+        type: "warning",
+        message: "已经登录，不能重复登录",
+      });
+      next(index);
+    } else {
+      //判断
+      if (getUser()) {
+        const role = getRole();
+        switch (role) {
+          case "ADMIN":
+            if (to.path.indexOf("/admin") === -1) {
+              ElMessage({
+                type: "warning",
+                message: "权限不足",
+              });
+              next(index);
+            } else {
+              next();
+            }
+            break;
+          case "STUDENT":
+            if (to.path.indexOf("/student") === -1) {
+              ElMessage({
+                type: "warning",
+                message: "权限不足",
+              });
+              next(index);
+            } else {
+              next();
+            }
+            break;
+          case "TEACHER":
+            if (to.path.indexOf("/teacher") === -1) {
+              ElMessage({
+                type: "warning",
+                message: "权限不足",
+              });
+              next(index);
+            } else {
+              next();
+            }
+            break;
+        }
+      } else {
+        //token异常了，就清除token
+        await rmToken();
+        next("/");
+      }
+    }
+  } else {
+    //未登录状态下
+    if (
+      to.path.indexOf("/student") !== -1 ||
+      to.path.indexOf("/admin") !== -1 ||
+      to.path.indexOf("/teacher") !== -1
+    ) {
+      ElMessage({
+        duration: 1000,
+        message: "当前尚未登录，请先登录",
+      });
+      next("/");
+    } else {
+      next();
+    }
+  }
+});
 
 export { router };
